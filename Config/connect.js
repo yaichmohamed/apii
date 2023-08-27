7bd927d37bd46ecab99c3956c6590558794ba093
yaichmohamed <127444596+yaichmohamed@users.noreply.github.com>
2023-08-27 18:58:15 +0200

    Update connect.js

--- Config/connect.js
+++ Config/connect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const connect= async()=>{
     const password= process.env.PASSWORD 
     try{
-        await mongoose.connect(`mongodb://samisaafi:mongodb+srv://samisaafi:${password}@cluster0.gs624vj.mongodb.net/?retryWrites=true&w=majority`)
+        await mongoose.connect(`mongodb://mohamed:mongodb+srv://samisaafi:${password}@cluster0.gs624vj.mongodb.net/?retryWrites=true&w=majority`)
         console.log('you are connected to ur db')
     }catch(err){
         console.log(err)
